efc19ab8892d194c4e8ec9fab629c5ec8ec118b6
Tobias <tobitgamer@gmail.com>
2023-12-07 12:19:30 -0300

    subiendo lo que tengo hasta la clase 18

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from django.template import Template, Context
+from django.http import HttpResponse
 # Create your views here.
+
+def inicio_view(xx):
+    return HttpResponse("<h3>Hola, buenas tardes(o noches)<h3/>")
+
+
+
+def curso_view(xx):
+    nombre = "Tobias"
+    edad = 30
+    apellido = "Barbero"
+
+    diccionario = {"nombre" : nombre,
+                   "edad" : edad,
+                   "apellido" : apellido,}
+    
+
+    ruta = "C:/Users/Usuario/Documents/Programacion/Python1/Proyecto-Coder-Final/AppCoder/templates/AppCoder/Padre.html"
+
+    
+    archivo = open(ruta, 'r')
+    
+    plantilla = Template(archivo.read())
+    contexto = Context(diccionario)
+    documento = plantilla.render(contexto)
+
+    return HttpResponse(documento)
+    
